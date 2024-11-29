a4bb4a942c7306c6c5d12062583001154ffdb8b9
Tielman <t@esterhuizens.org>
2024-11-29 14:22:55 +1300

    Update 1_sol_install-jito.sh

--- 1_sol_install-jito.sh
+++ 1_sol_install-jito.sh
@@ -13,6 +13,3 @@ cd jito-solana
 git checkout tags/$TAG
 git submodule update --init --recursive
 CI_COMMIT=$(git rev-parse HEAD) scripts/cargo-install-all.sh --validator-only ~/.local/share/solana/install/releases/"$TAG"
-
-
-# test
