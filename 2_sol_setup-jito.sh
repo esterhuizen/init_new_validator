ed215dfb4a4e939159468bb68a84ed5a9107ccc9
Tielman <t@esterhuizens.org>
2024-12-01 16:55:12 +1300

    Update 2_sol_setup-jito.sh

--- 2_sol_setup-jito.sh
+++ 2_sol_setup-jito.sh
@@ -1,3 +1,32 @@
+curl https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
+rustup component add rustfmt
+rustup update
+sudo apt-get update
+sudo apt-get install libssl-dev libudev-dev pkg-config zlib1g-dev llvm clang cmake make libprotobuf-dev protobuf-compiler
+echo
+
+printf "What version of jito?: "
+read TAG
+export $TAG
+export TAG=v2.0.15-jito
+git clone https://github.com/jito-foundation/jito-solana.git --recurse-submodules
+cd jito-solana
+git checkout tags/$TAG
+git submodule update --init --recursive
+CI_COMMIT=$(git rev-parse HEAD) scripts/cargo-install-all.sh --validator-only ~/.local/share/solana/install/releases/"$TAG"
+
+echo
+printf "what should be appended to .profile?: "
+read top
+
+echo $top >> ~/.profile
+. ~/.profile
+
+
+
+
+
 mkdir ~/validator_run_env/ 
 cd ~/validator_run_env/
 
