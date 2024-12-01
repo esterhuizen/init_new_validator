8e8f2269a5961b4b2fd8840013f8adca903982cb
Tielman <t@esterhuizens.org>
2024-12-01 17:20:18 +1300

    Update 1_sol_setup-jito.sh

--- 1_sol_setup-jito.sh
+++ 1_sol_setup-jito.sh
@@ -65,3 +65,17 @@ solana create-vote-account -u$net \
     ./vote-account-keypair.json \
     ./validator-keypair.json \
     ./authorized-withdrawer-keypair.json
+
+    
+bdir=/home/sol/validator_run_env/bin
+ldir=/home/sol/validator_run_env/log
+
+mkdir $bdir $ldir
+
+mv start-mainnet-jito-NY.sh-2.0.15 $bdir
+mv stop-validator.sh $bdir
+
+chmod 700 $bdir/*
+
+./check_response.sh
+
