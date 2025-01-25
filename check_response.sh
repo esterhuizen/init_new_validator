d7355eb8c803cd23101d1ae463255f96424a46ec
SolStrat <t@esterhuizens.org>
2025-01-25 19:35:59 +1300

    Update check_response.sh

--- check_response.sh
+++ check_response.sh
@@ -1,4 +1,6 @@
 # for i in `cat servers.txt`; do echo $i | sed 's|https://||'; time curl $i; sleep 1; done
 
-for i in `cat servers.txt`; do echo $i; time curl $i; echo ===========; sleep 1; done
+serverf=$(dirname $0)/servers.txt
+
+for i in `cat $serverf`; do echo $i; time curl $i; echo ===========; sleep 1; done
 
