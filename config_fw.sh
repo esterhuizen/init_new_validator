8a231ec03a25a5a1241836dabe1af4fc18c5731e
Web3-wild <t@esterhuizens.org>
2025-06-09 13:09:54 +1200

    Create config_fw.sh

---
+++ config_fw.sh
@@ -0,0 +1,11 @@
+sudo ufw allow 9000:10000/udp
+sudo ufw allow 9000:10000/tcp
+sudo ufw allow 11226/udp
+sudo ufw allow 11226/tcp
+sudo ufw allow 8000:8020/udp
+sudo ufw allow 8000:8020/tcp
+sudo ufw allow 22/tcp
+sudo ufw allow 11228/udp
+sudo ufw allow 11229/udp
+sudo ufw allow 1022/tcp
+sudo ufw enable
